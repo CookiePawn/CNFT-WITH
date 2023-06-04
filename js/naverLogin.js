52635cd7c1099c8fa527eeee06093a573d7df3bd
Moo-Hee <dngml3690@naver.com>
2023-06-04 19:00:55 +0900

    EDIT 1. 서버 업로드용 콜백URL, DB 비밀번호 등 수정

--- js/naverLogin.js
+++ js/naverLogin.js
@@ -1,7 +1,7 @@
 var naverLogin = new naver.LoginWithNaverId(
    {
       clientId: "486PJdxDEgfuF5VsZOMU",
-      callbackUrl: "http://localhost:3000/naverLoginCheck", // http://cookiepawn.shop/naverLoginCheck
+      callbackUrl: "http://cookiepawn.shop/naverLoginCheck", // http://cookiepawn.shop/naverLoginCheck
       isPopup: false,
       callbackHandle: true
    }
