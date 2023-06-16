4489d17a125a36fd7ada914d3f14021dfe0c2361
Moo-Hee <dngml3690@naver.com>
2023-06-16 20:31:48 +0900

    UPDATE 1. SSL 인증 완료 2. 그에 따른 코드 내 URL 수정

--- js/naverLogin.js
+++ js/naverLogin.js
@@ -1,7 +1,7 @@
 var naverLogin = new naver.LoginWithNaverId(
    {
       clientId: "486PJdxDEgfuF5VsZOMU",
-      callbackUrl: "http://cookiepawn.shop/naverLoginCheck", // http://cookiepawn.shop/naverLoginCheck
+      callbackUrl: "https://cookiepawn.shop/naverLoginCheck", // https://cookiepawn.shop/naverLoginCheck
       isPopup: false,
       callbackHandle: true
    }
